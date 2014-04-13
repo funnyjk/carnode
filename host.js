4e164589f270c6f53ba7d5aa8aba5e5714bd4875
funnyjk <funnyjk@funnyjk-1000HE.(none)>
2014-04-12 20:48:39 -0400

    fixed host.js

--- host.js
+++ host.js
@@ -1,40 +1,84 @@
-var	
-	express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(5000), 
+var 
+  express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(5000),
+  serialport = require("serialport"),
+  SerialPort = serialport.SerialPort, // localize object constructor
+  fs = require('fs');
 
-//setting up http stuff
+//SERIAL
+var portName = '/dev/ttyACM0';
+var serial = new SerialPort(portName, {
+   baudRate: 9600,
+   dataBits: 8, 
+   parity: 'none', 
+   stopBits: 1, 
+   flowControl: false 
+});
+  
+var lightOn = false;
 
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
   app.use(express.static(__dirname + '/assets'));
 });
-
+  
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
-});
-
-app.get('/client', function(req, res){
-  res.sendfile(__dirname + '/client.html');
-});
-
+})
+  
 var port = 3000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
-
-io.sockets.on('connection', function (socket) {
-  
-    socket.on('switch', function(state) (
-      if (state == "forward"){
-	forward();
-      }
-      if (state == "backward"){
-	backward();
-      }
-    
-    socket.on('test', function (data){
-      console.log(data);
+ 
+function serial_start() {
+  serial.on("open", function () {
+      console.log('open serial communication');
     });
+}
+
+serial_start();
+
+function forward() {
+  serial.write('f');
+}
+
+function backward() {
+  serial.write('b');
+}
+function left() {
+  serial.write('l');
+}
+
+function right() {
+  serial.write('r');
+}
+function stop() {
+  serial.write('s');
+}
+
+io.sockets.on('connection', function(socket) {
+  
+  socket.on('switch', function(state) {
+    if (state == "forward"){
+      forward();
+    }
+    if (state == "backward") {
+      backward();
+    }
+     if (state == "left"){
+      left();
+    }
+    if (state == "right") {
+      right();
+    }
+     if (state == "stop"){
+      stop();
+    }
+  });
+  socket.on('test', function(data) {
+    console.log(data);
+  });
 });
